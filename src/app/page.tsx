a72aff0f6cf56f790d1be6f42f06b216ed5a011b
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-02-05 01:46:47 +0200

    feat/hero-image-animation

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import Header from "@/sections/Header";
+import Hero from "@/sections/Hero";
+import Intro from "@/sections/Intro";
+import Projects from "@/sections/Projects";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Intro />
+      <Projects />
+    </>
+    // <h1 className="text-9xl">
+    //   Crafting digital experiences through code and creative design
+    // </h1>
+  );
+}
