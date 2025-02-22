55e96fc583c75e9a65d6e8d4e25a411dc20aee13
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-02-22 06:26:58 +0200

    feat/hero-start

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -7,11 +7,11 @@ import { AnimatePresence, motion, useAnimate } from "framer-motion";
 import NavMenu from "@/components/NavMenu";
 
 const navItems = [
-  { label: "About", href: "#intro" },
-  { label: "Selected Works", href: "#projects" },
-  { label: "Testimonials", href: "#testimonials" },
-  { label: "FAQs", href: "#faqs" },
-  { label: "Contact", href: "#contact" },
+  { label: "About", href: "#intro", color: "#e49366" },
+  { label: "Selected Works", href: "#projects", color: "#798e7b" },
+  { label: "Testimonials", href: "#testimonials", color: "#b692a1" },
+  { label: "FAQs", href: "#faqs", color: "#bfccd8" },
+  { label: "Contact", href: "#contact", color: "#025380" },
 ];
 
 const Header: FC = () => {
@@ -56,17 +56,72 @@ const Header: FC = () => {
     animateBurger();
   }, [isOpen, animate, scope]);
 
-  const items = navItems.map(({ label, href }) => (
-    <li key={href} className="group p-4">
-      <Link href={href} passHref>
-        <Button variant="text" className="w-full md:inline-flex">
-          <span className="text-base text-white capitalize hover:text-opacity-80 transition-all">
-            {label}
-          </span>
-        </Button>
-      </Link>
-    </li>
-  ));
+  const items = navItems.map(({ label, href, color }) => {
+    const circleVariants = {
+      initial: { width: "0.67em", height: "0.67em" },
+      hover: { width: "1.9em", height: "1.9em" },
+    };
+
+    const arrowVariants = {
+      initial: { opacity: 0, x: -5, y: 5 },
+      hover: {
+        opacity: 1,
+        x: 0,
+        y: 0,
+        transition: { delay: 0.2 },
+      },
+    };
+
+    return (
+      <motion.li
+        key={href}
+        className="relative flex items-center gap-1 p-4"
+        whileHover="hover"
+        initial="initial"
+      >
+        <div
+          className="relative flex items-center justify-center w-[1.9em] h-[1.9em] cursor-pointer overflow-hidden"
+          style={{ borderRadius: "50%" }}
+        >
+          <motion.div
+            className="absolute flex items-center justify-center"
+            variants={circleVariants}
+            transition={{ duration: 0.3, ease: "easeInOut" }}
+            style={{ backgroundColor: color, borderRadius: "50%" }}
+          >
+            <motion.span
+              className="AppSvg HomeHeroMenu-itemArrowIcon --arrow"
+              variants={arrowVariants}
+              transition={{ duration: 0.2 }}
+            >
+              <svg
+                width="9"
+                height="9"
+                viewBox="0 0 19 19"
+                fill="inherit"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M0.292893 17.2929C-0.0976311 17.6834 -0.0976311 18.3166 0.292893 18.7071C0.683418 19.0976 1.31658 19.0976 1.70711 18.7071L0.292893 17.2929ZM18.9706 1.02944C18.9706 0.477153 18.5228 0.0294373 17.9706 0.029437L8.97056 0.0294378C8.41828 0.0294375 7.97056 0.477153 7.97056 1.02944C7.97056 1.58172 8.41828 2.02944 8.97056 2.02944L16.9706 2.02944L16.9706 10.0294C16.9706 10.5817 17.4183 11.0294 17.9706 11.0294C18.5228 11.0294 18.9706 10.5817 18.9706 10.0294L18.9706 1.02944ZM1.70711 18.7071L18.6777 1.73654L17.2635 0.322331L0.292893 17.2929L1.70711 18.7071Z"
+                  fill="inherit"
+                ></path>
+              </svg>
+            </motion.span>
+          </motion.div>
+        </div>
+        <Link href={href} passHref>
+          <Button
+            variant="text"
+            className="w-full md:inline-flex !no-underline hover:!no-underline"
+          >
+            <span className="text-base text-white capitalize transition-all no-underline">
+              {label}
+            </span>
+          </Button>
+        </Link>
+      </motion.li>
+    );
+  });
 
   return (
     <header>
@@ -95,7 +150,7 @@ const Header: FC = () => {
               <div className="flex justify-end h-20 items-center">
                 <div className="flex items-center gap-4">
                   <motion.div
-                    className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-stone-200 cursor-pointer"
+                    className="size-11 border border-stone-400 rounded-full inline-flex items-center justify-center bg-[#b692a1] cursor-pointer"
                     onClick={() => setIsOpen(!isOpen)}
                     whileHover={{ scale: 1.05 }}
                     ref={scope}
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { FC, useEffect, useRef } from "react";
-import Button from "@/components/Button";
+// import Button from "@/components/Button";
 import SplitType from "split-type";
 import {
   useAnimate,
@@ -10,7 +10,8 @@ import {
   useScroll,
   useTransform,
 } from "framer-motion";
-import Image from "next/image";
+import { dividerMotion } from "@/utils/animations";
+// import Image from "next/image";
 
 const Hero: FC = () => {
   const [titleScope, titleAnimate] = useAnimate();
@@ -26,11 +27,11 @@ const Hero: FC = () => {
     [0, 1],
     ["50vw", "100vw"]
   );
-  const portraitHeight = useTransform(
-    scrollYProgress,
-    [0, 1],
-    ["50vh", "100vh"]
-  );
+  // const portraitHeight = useTransform(
+  //   scrollYProgress,
+  //   [0, 1],
+  //   ["50vh", "100vh"]
+  // );
 
   useEffect(() => {
     new SplitType(titleScope.current, {
@@ -61,11 +62,9 @@ const Hero: FC = () => {
               className="text-5xl md:text-6xl lg:text-8xl mt-40 md:mt-0"
               ref={titleScope}
             >
-              {/* Crafting digital experiences through code and creative design */}
               Full-Stack Developer
             </motion.h1>
-            <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
-              {/* <motion.div */}
+            <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6 justify-between">
               <motion.h2
                 initial={{ opacity: 0, y: "100%" }}
                 animate={{ opacity: 1, y: 0 }}
@@ -77,65 +76,20 @@ const Hero: FC = () => {
                 I specialize in turning digital ideas into user-friendly
                 experiences, whether crafting sleek websites or seamless mobile
                 apps. We blend creativity with tech savvy.
-                {/* <Button
-                  variant="secondary"
-                  iconAfter={
-                    <div className="overflow-hidden size-5">
-                      <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                          />
-                        </svg>
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth="1.5"
-                          stroke="currentColor"
-                          className="size-5"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-                          />
-                        </svg>
-                      </div>
-                    </div>
-                  }
-                >
-                  <span>View my Work</span>
-                </Button>
-              </motion.div>
-              <motion.div
-                initial={{ opacity: 0, y: "100%" }}
-                animate={{ opacity: 1, y: 0 }}
-                transition={{
-                  duration: 0.5,
-                  delay: 2.2,
-                }}
-              >
-                <Button variant="text">Let&apos;s talk</Button>*/}
               </motion.h2>
+              {/* <motion.div
+                className=" bottom-20 h-[2px] bg-black w-[100%] origin-left"
+                variants={dividerMotion}
+              /> */}
             </div>
           </div>
         </div>
+
         <div className="md:col-span-6 relative">
-          {/* <motion.div
-            className="mt-20 md:mt-0 md:absolute md:right-0 md:top-0"
+          <motion.div
+            className="mt-20 md:mt-0 md:size-full md:absolute md:right-0 max-md:!w-full bg-[#b692a1]"
             style={{
               width: portraitWidth,
-              height: portraitHeight,
             }}
           >
             <video
@@ -146,18 +100,10 @@ const Hero: FC = () => {
               loop
               muted
             />
-          </motion.div> */}
-          <motion.div
-            className="mt-20 md:mt-0 md:size-full md:absolute md:right-0 max-md:!w-full bg-black"
-            style={{
-              width: portraitWidth,
-            }}
-          >
-            {/* <Image
-              src={heroImage}
-              alt="My portrait"
-              className="size-full object-cover"
-            /> */}
+
+            <h2 className="absolute bottom-10 right-10 text-5xl md:text-[70px] text-white font-thin">
+              ROMAN RYABCHINSKIY
+            </h2>
           </motion.div>
         </div>
       </div>
@@ -168,15 +114,9 @@ const Hero: FC = () => {
 
 export default Hero;
 
-//-------------------- origin -------------------------------------------------
 // "use client";
 
 // import { FC, useEffect, useRef } from "react";
-// /* eslint-disable-next-line @typescript-eslint/no-unused-vars */
-// /* eslint-disable-next-line */
-// import heroImage from "@/assets/images/hero-image.jpg";
-// import Image from "next/image";
-// import Button from "@/components/Button";
 // import SplitType from "split-type";
 // import {
 //   useAnimate,
@@ -184,9 +124,7 @@ export default Hero;
 //   stagger,
 //   useScroll,
 //   useTransform,
-// } from "motion/react";
-// import { div } from "motion/react-client";
-// // import { transform } from "motion";
+// } from "framer-motion";
 
 // const Hero: FC = () => {
 //   const [titleScope, titleAnimate] = useAnimate();
@@ -197,8 +135,16 @@ export default Hero;
 //     offset: ["start end", "end end"],
 //   });
 
-//   const portraitWidth = useTransform(scrollYProgress, [0, 1], ["100%", "240%"]);
-//   // 12 /5 = 2.4 * 100 = 240%
+//   const portraitWidth = useTransform(
+//     scrollYProgress,
+//     [0, 1],
+//     ["50vw", "100vw"]
+//   );
+//   const portraitHeight = useTransform(
+//     scrollYProgress,
+//     [0, 1],
+//     ["calc(50vh - 50%)", "100vh"]
+//   );
 
 //   useEffect(() => {
 //     new SplitType(titleScope.current, {
@@ -217,92 +163,56 @@ export default Hero;
 //       }
 //     );
 //   }, []);
+
 //   return (
 //     <section>
 //       <div className="grid md:grid-cols-12 md:h-screen items-stretch sticky top-0">
-//         <div className="md:col-span-7 flex flex-col justify-center">
+//         <div className="md:col-span-6 flex flex-col justify-center relative z-10 mix-blend-difference">
 //           <div className="container !max-w-full">
 //             <motion.h1
 //               initial={{ opacity: 0 }}
 //               animate={{ opacity: 1 }}
-//               className="text-5xl md:text-6xl lg:text-7xl mt-40 md:mt-0"
+//               className="text-5xl md:text-6xl lg:text-8xl mt-40 md:mt-0"
 //               ref={titleScope}
 //             >
-//               Crafting digital experiences through code and creative design
+//               {/* Crafting digital experiences through code and creative design */}
+//               Full-Stack Developer
 //             </motion.h1>
 //             <div className="flex flex-col md:flex-row md:items-center mt-10 items-start gap-6">
-//               <motion.div
+//               {/* <motion.div */}
+//               <motion.h2
 //                 initial={{ opacity: 0, y: "100%" }}
 //                 animate={{ opacity: 1, y: 0 }}
 //                 transition={{
 //                   duration: 0.5,
-//                   delay: 1.75,
+//                   delay: 1,
 //                 }}
 //               >
-//                 <Button
-//                   variant="secondary"
-//                   iconAfter={
-//                     <div className="overflow-hidden size-5">
-//                       <div className="h-5 w-10 flex group-hover/button:-translate-x-1/2 transition-transform duration-500">
-//                         <svg
-//                           xmlns="http://www.w3.org/2000/svg"
-//                           fill="none"
-//                           viewBox="0 0 24 24"
-//                           strokeWidth="1.5"
-//                           stroke="currentColor"
-//                           className="size-5"
-//                         >
-//                           <path
-//                             strokeLinecap="round"
-//                             strokeLinejoin="round"
-//                             d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-//                           />
-//                         </svg>
-//                         <svg
-//                           xmlns="http://www.w3.org/2000/svg"
-//                           fill="none"
-//                           viewBox="0 0 24 24"
-//                           strokeWidth="1.5"
-//                           stroke="currentColor"
-//                           className="size-5"
-//                         >
-//                           <path
-//                             strokeLinecap="round"
-//                             strokeLinejoin="round"
-//                             d="m4.5 5.25 7.5 7.5 7.5-7.5m-15 6 7.5 7.5 7.5-7.5"
-//                           />
-//                         </svg>
-//                       </div>
-//                     </div>
-//                   }
-//                 >
-//                   <span>View my Work</span>
-//                 </Button>
-//               </motion.div>
-//               <motion.div
-//                 initial={{ opacity: 0, y: "100%" }}
-//                 animate={{ opacity: 1, y: 0 }}
-//                 transition={{
-//                   duration: 0.5,
-//                   delay: 2.2,
-//                 }}
-//               >
-//                 <Button variant="text">Let&apos;s talk</Button>
-//               </motion.div>
+//                 I specialize in turning digital ideas into user-friendly
+//                 experiences, whether crafting sleek websites or seamless mobile
+//                 apps. We blend creativity with tech savvy.
+//               </motion.h2>
 //             </div>
 //           </div>
 //         </div>
-//         <div className="md:col-span-5 relative">
+
+//         <div className="md:col-span-6 relative ">
 //           <motion.div
-//             className="mt-20 md:mt-0 md:size-full md:absolute md:right-0 max-md:!w-full"
+//             className="mt-20 md:mt-0 md:size-full md:absolute md:right-0 max-md:!w-full bg-[#798e7b] top-1/2 -translate-y-1/2"
 //             style={{
 //               width: portraitWidth,
+//               height: portraitHeight,
 //             }}
 //           >
-//             <Image
-//               src={heroImage}
-//               alt="My portrait"
+//             {/* Фон с явным указанием размеров */}
+//             <div className="absolute inset-0 bg-[#798e7b] -z-10 w-full h-screen" />
+
+//             <video
+//               src="/videos/13522169-hd_1920_1080_25fps.mp4"
 //               className="size-full object-cover"
+//               autoPlay
+//               loop
+//               muted
 //             />
 //           </motion.div>
 //         </div>
