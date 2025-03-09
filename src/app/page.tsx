75568ac3af77e884084f2dbefa8114a99f43c029
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-03-09 21:25:04 +0200

    feat/added-lenis-scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { useEffect } from "react";
+// import Lenis from "@studio-freight/lenis";
+import Lenis from "lenis";
 // import FAQs from "@/sections/FAQs";
 import Header from "@/sections/Header";
 import Hero from "@/sections/Hero";
@@ -6,6 +11,16 @@ import Projects from "@/sections/Projects";
 import MyServices from "@/sections/MyServices";
 
 export default function Home() {
+  useEffect(() => {
+    const lenis = new Lenis();
+    function raf(time: any) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+  }, []);
+
   return (
     <>
       <Header />
@@ -17,3 +32,25 @@ export default function Home() {
     </>
   );
 }
+
+//
+//-------------------------- init ---------------------------------------
+// // import FAQs from "@/sections/FAQs";
+// import Header from "@/sections/Header";
+// import Hero from "@/sections/Hero";
+// import Intro from "@/sections/Intro";
+// import Projects from "@/sections/Projects";
+// import MyServices from "@/sections/MyServices";
+
+// export default function Home() {
+//   return (
+//     <>
+//       <Header />
+//       <Hero />
+//       <MyServices />
+//       {/* <FAQs /> */}
+//       <Projects />
+//       <Intro />
+//     </>
+//   );
+// }
