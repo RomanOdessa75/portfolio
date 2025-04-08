92a851c5216aed4210c285b2d8a64ac37b72d0f4
RomanOdessa75 <115027650+RomanOdessa75@users.noreply.github.com>
2025-04-08 16:31:13 +0300

    fix: earth disabled

--- src/components/Earth.tsx
+++ src/components/Earth.tsx
@@ -1,51 +1,52 @@
 // Earth.tsx ('use client' компонент)
 "use client";
 
-import { Canvas, useLoader } from "@react-three/fiber";
-import { useRef } from "react";
-import { useScroll } from "framer-motion";
-import { TextureLoader } from "three";
-import { motion } from "framer-motion-3d";
+// import { Canvas, useLoader } from "@react-three/fiber";
+// import { useRef } from "react";
+// import { useScroll } from "framer-motion";
+// import { TextureLoader } from "three";
+// import { motion } from "framer-motion-3d";
 
 const Earth = () => {
-  const sceneRef = useRef(null);
-  
-  const { scrollYProgress } = useScroll({
-    target: sceneRef,
-    offset: ["start end", "end start"],
-  });
+  // const sceneRef = useRef(null);
 
-  const [color, normal, aoMap] = useLoader(TextureLoader, [
-    "/assets/color.jpg",
-    "/assets/normal.png",
-    "/assets/occlusion.jpg",
-  ]);
+  // const { scrollYProgress } = useScroll({
+  //   target: sceneRef,
+  //   offset: ["start end", "end start"],
+  // });
+
+  // const [color, normal, aoMap] = useLoader(TextureLoader, [
+  //   "/assets/color.jpg",
+  //   "/assets/normal.png",
+  //   "/assets/occlusion.jpg",
+  // ]);
 
   return (
-    <div ref={sceneRef} className="w-full h-full">
-      <Canvas
-        style={{
-          width: "100%",
-          height: "100%",
-        }}
-        camera={{ position: [0, 0, 5], fov: 60 }}
-      >
-        <ambientLight intensity={0.1} />
-        <directionalLight intensity={3.5} position={[1, 0, -0.25]} />
-        <motion.mesh
-          scale={2.5}
-          rotation-y={scrollYProgress}
-          position={[0, 0, 0]}
-        >
-          <sphereGeometry args={[1, 64, 64]} />
-          <meshStandardMaterial
-            map={color}
-            normalMap={normal}
-            aoMap={aoMap}
-          />
-        </motion.mesh>
-      </Canvas>
-    </div>
+    <></>
+    // <div ref={sceneRef} className="w-full h-full">
+    //   <Canvas
+    //     style={{
+    //       width: "100%",
+    //       height: "100%",
+    //     }}
+    //     camera={{ position: [0, 0, 5], fov: 60 }}
+    //   >
+    //     <ambientLight intensity={0.1} />
+    //     <directionalLight intensity={3.5} position={[1, 0, -0.25]} />
+    //     <motion.mesh
+    //       scale={2.5}
+    //       rotation-y={scrollYProgress}
+    //       position={[0, 0, 0]}
+    //     >
+    //       <sphereGeometry args={[1, 64, 64]} />
+    //       <meshStandardMaterial
+    //         map={color}
+    //         normalMap={normal}
+    //         aoMap={aoMap}
+    //       />
+    //     </motion.mesh>
+    //   </Canvas>
+    // </div>
   );
 };
 
